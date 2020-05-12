534a5613fca74c3b31804d06c8fa06e71bffb4de
Rix <rix.turcan@gmail.com>
2020-05-12 16:56:02 +0200

    Minor fix on setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lotery-drawer",
-    version="1.0.0",
+    version="1.0.1",
     author="Crypt Online",
     author_email="admin@hxrobot.io",
     description="A simple, proven and non-falsifiable lottery draw tool",
--- upload.bat
+++ upload.bat
@@ -1,2 +1,3 @@
+del dist\*.*?
 py setup.py sdist bdist_wheel
 py -m twine upload --repository pypi dist/* --config-file .twine
